9168224c7b9b8d641263426f475bcef4487fe85f
TylerDewey <ty.dewey@gmail.com>
2014-01-24 10:42:33 -0600

    Added test2 method

---
+++ src/taojava/labs/testing/C2FTest.java
@@ -0,0 +1,23 @@
+package taojava.labs.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class C2FTest
+{
+
+  @Test
+  public void
+    testC2f ()
+  {
+    assertEquals (0, 0);
+  }
+
+  @Test
+  public void
+    test2 ()
+  {
+    assertEquals ("stupid test", 10, 3 * 5);
+  }// test2()
+}
